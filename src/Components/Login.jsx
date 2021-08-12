d763018c29279b0096f4c2a622df3e47e9912f8b
Atul Saini <atulsaini9886@gmail.com>
2021-08-12 15:28:57 +0530

    Designed Login Page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { FaGithub, FaGooglePlusG } from "react-icons/fa";
 function Login() {
   return (
-    <div className="login-page">
-      <div className="login-logo-image">
-        <img src="" alt="logo-image" />
-      </div>
-      <div className="login-options">
-        <div className="login-with-google">
-          <FaGooglePlusG /> Login With Google
+    <div className="login-full">
+      <div className="login-page">
+        <div className="login-logo-image">
+          <img src="" alt="logo-image" />
         </div>
-        <div className="login-with-github">
-          <FaGithub /> Login With Github
+        <div className="login-options">
+          <div className="login-with-google login-with">
+            <FaGooglePlusG className="icon-login" /> <p>Login With Google</p>
+          </div>
+          <div className="login-with-github login-with">
+            <FaGithub className="icon-login" /> <p>Login With Github</p>
+          </div>
         </div>
       </div>
     </div>
